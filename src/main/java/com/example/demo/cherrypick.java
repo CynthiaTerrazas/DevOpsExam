2ceb1e1af90dbfef8fec0e0ba377eafa73b32188
cynthia terrazas <cv.terrazasgutierrez@gmail.com>
2018-07-06 18:51:19 -0700

    segundo commit

--- src/main/java/com/example/demo/cherrypick.java
+++ src/main/java/com/example/demo/cherrypick.java
@@ -5,4 +5,8 @@ public class cherrypick {
     public void primercommit() {
         System.out.printf("primer commir");
     }
+
+    public void segundocommit() {
+        System.out.printf("segundo commir");
+    }
 }
