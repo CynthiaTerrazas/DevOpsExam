67ce5780b9236d233109de2cdbabb83760950bee
cynthia terrazas <cv.terrazasgutierrez@gmail.com>
2018-07-06 18:51:19 -0700

    cherry pick

---
+++ src/main/java/com/example/demo/cherrypick.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class cherrypick {
+
+    public void primercommit() {
+        System.out.printf("primer commir");
+    }
+
+    public void segundocommit() {
+        System.out.printf("segundo commir");
+    }
+}
